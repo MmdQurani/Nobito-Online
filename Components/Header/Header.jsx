e113321b1499c2f73a5b4125b4145d80d30f1ec2
78 mmd 69 <mmdqurani@gmail.com>
2025-04-30 23:00:06 +0330

    Implement header section only for desktop modes

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -1,34 +1,44 @@
-import Link from 'next/link'
 import React from 'react'
+import Link from 'next/link'
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { IoMenu } from "react-icons/io5";
 
 function Header() {
   return (
     <header className='h-36 w-full sticky top-0 mb-8 mx-auto bg-white'>
 
-      <div className='container w-full h-full mx-auto flex flex-col'>
+      <div className='container w-8xl h-full mx-auto flex flex-col'>
         <div className='top-bar h-[70%] flex items-center'>
 
           <nav className='w-full flex flex-row justify-between items-center' dir='rtl'>
-            <div className='nav-right flex flex-row items-center bg-blue-400'>
+            <div className='nav-right flex flex-row items-center'>
 
               <div className='logo-box'>
                 <img src="/images/logo/Logo.png" className='w-full' alt="" />
               </div>
 
-              <div className='navbar-btns h-full flex flex-row gap-x-4 ms-8'>
-                <Link href='/' className='px-4 py-1 rounded-sm font-Dirooz font-normal text-sub-text'>نوبت دهی مطب</Link>
-                <Link href='/' className='px-4 py-1 rounded-sm font-Dirooz font-normal text-sub-text'>خدمات</Link>
-                <Link href='/' className='px-4 py-1 rounded-sm font-Dirooz font-normal text-sub-text'>مشاوره آنلاین</Link>
-                <Link href='/' className='px-4 py-1 rounded-sm font-Dirooz font-normal text-sub-text'>مجله سلامت</Link>
-                <Link href='/' className='px-4 py-1 rounded-sm font-Dirooz font-normal text-sub-text'>نیکوکاری</Link>
+              <div className='navbar-btns h-full lg:flex flex-row gap-x-2 hidden ms-4'>
+                <Link href='/' className='px-2.5 py-1 rounded-sm font-Dirooz font-normal text-sm hover:text-primery-600 text-sub-text'>نوبت دهی مطب</Link>
+                <Link href='/' className='px-2.5 py-1 rounded-sm font-Dirooz font-normal text-sm hover:text-primery-600 text-sub-text'>خدمات</Link>
+                <Link href='/' className='px-2.5 py-1 rounded-sm font-Dirooz font-normal text-sm hover:text-primery-600 text-sub-text'>مشاوره آنلاین</Link>
+                <Link href='/' className='px-2.5 py-1 rounded-sm font-Dirooz font-normal text-sm hover:text-primery-600 text-sub-text'>مجله سلامت</Link>
+                <Link href='/' className='px-2.5 py-1 rounded-sm font-Dirooz font-normal text-sm hover:text-primery-600 text-sub-text'>نیکوکاری</Link>
               </div>
 
             </div>
 
-            <div className='nav-left flex-1 bg-red-400'>
-              <div className='prof-btns-box flex flex-row justify-end'>
-                <Link href='/' className='px-4 py-1 rounded-sm font-Dirooz font-normal bg-amber-300 text-sub-text'>نیکوکاری</Link>
-                <Link href='/' className='px-4 py-1 rounded-sm font-Vazir font-bold bg-amber-300 text-sub-text'>ورود/ثبت نام</Link>
+            <div className='nav-left'>
+              <div className='prof-btns-box lg:flex flex-row justify-end gap-x-2 hidden'>
+                <Link href='/' className='flex items-center justify-center rounded-sm font-Dirooz font-normal text-sub-text'>
+                <IoMdNotificationsOutline className='text-4xl text-sub-text hover:text-primery-600' />
+                </Link>
+                <Link href='/' className='flex justify-center items-center px-4 py-2.5 rounded-md font-Vazir font-bold text-sm bg-primery-600 text-white'>ورود / ثبت نام</Link>
+              </div>
+
+              <div className='lg:hidden flex justify-end'>
+                <button className='cursor-pointer'>
+                  <IoMenu className='text-4xl text-sub-text hover:text-primery-600' />
+                </button>
               </div>
             </div>
           </nav>
