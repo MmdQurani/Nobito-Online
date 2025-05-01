b0286deb8f9a3fd77d4650e9f9b2a36a11c9ca69
78 mmd 69 <mmdqurani@gmail.com>
2025-05-01 12:36:42 +0330

    Base component to display all header section items

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -13,42 +13,80 @@ function Header() {
           <nav className='w-full flex flex-row justify-between items-center' dir='rtl'>
             <div className='nav-right flex flex-row items-center'>
 
-              <div className='logo-box'>
-                <img src="/images/logo/Logo.png" className='w-full' alt="" />
-              </div>
+              <LogoBox />
 
-              <div className='navbar-btns h-full lg:flex flex-row gap-x-2 hidden ms-4'>
-                <Link href='/' className='px-2.5 py-1 rounded-sm font-Dirooz font-normal text-sm hover:text-primery-600 text-sub-text'>نوبت دهی مطب</Link>
-                <Link href='/' className='px-2.5 py-1 rounded-sm font-Dirooz font-normal text-sm hover:text-primery-600 text-sub-text'>خدمات</Link>
-                <Link href='/' className='px-2.5 py-1 rounded-sm font-Dirooz font-normal text-sm hover:text-primery-600 text-sub-text'>مشاوره آنلاین</Link>
-                <Link href='/' className='px-2.5 py-1 rounded-sm font-Dirooz font-normal text-sm hover:text-primery-600 text-sub-text'>مجله سلامت</Link>
-                <Link href='/' className='px-2.5 py-1 rounded-sm font-Dirooz font-normal text-sm hover:text-primery-600 text-sub-text'>نیکوکاری</Link>
-              </div>
+              <NavbarBtnsBox />
 
             </div>
 
             <div className='nav-left'>
-              <div className='prof-btns-box lg:flex flex-row justify-end gap-x-2 hidden'>
-                <Link href='/' className='flex items-center justify-center rounded-sm font-Dirooz font-normal text-sub-text'>
-                <IoMdNotificationsOutline className='text-4xl text-sub-text hover:text-primery-600' />
-                </Link>
-                <Link href='/' className='flex justify-center items-center px-4 py-2.5 rounded-md font-Vazir font-bold text-sm bg-primery-600 text-white'>ورود / ثبت نام</Link>
-              </div>
 
-              <div className='lg:hidden flex justify-end'>
-                <button className='cursor-pointer'>
-                  <IoMenu className='text-4xl text-sub-text hover:text-primery-600' />
-                </button>
-              </div>
+              <ProfBtnsBox />
+              
+              <HamburgerMenu />
+
             </div>
           </nav>
 
         </div>
 
         <div className='bottom-bar bg-cyan-500 flex-1'>hello world</div>
+        
       </div>
 
     </header>
+  )
+}
+
+export function LogoBox() {
+  return (
+    <div className='logo-box'>
+      <img src="/images/logo/Logo.png" className='w-full' alt="" />
+    </div>
+  )
+}
+
+export function NavbarBtnsBox() {
+  return (
+    <div className='navbar-btns h-full lg:flex flex-row gap-x-2 hidden ms-4'>
+      <NavbarBtn title='نوبت دهی مطب' />
+      <NavbarBtn title='خدمات' />
+      <NavbarBtn title='مشاوره آنلاین' />
+      <NavbarBtn title='مجله سلامت' />
+      <NavbarBtn title='نیکوکاری' />
+    </div>
+  )
+}
+
+export function NavbarBtn({ title }) {
+  return (
+    <Link href='/' className='px-2.5 py-1 rounded-sm font-Dirooz font-normal text-sm hover:text-primery-600 text-sub-text'>
+      {title}
+    </Link>
+  )
+}
+
+export function ProfBtnsBox() {
+  return (
+    <div className='prof-btns-box lg:flex flex-row justify-end gap-x-2 hidden'>
+
+      <Link href='/' className='flex items-center justify-center rounded-sm font-Dirooz font-normal text-sub-text'>
+        <IoMdNotificationsOutline className='text-4xl text-sub-text hover:text-primery-600' />
+      </Link>
+
+      <Link href='/' className='flex justify-center items-center px-4 py-2.5 rounded-md font-Vazir font-bold text-sm bg-primery-600 text-white'>ورود / ثبت نام</Link>
+
+    </div>
+  )
+}
+
+export function HamburgerMenu() {
+  return (
+    <div className='lg:hidden flex justify-end'>
+      <button className='cursor-pointer'>
+        <IoMenu className='text-4xl text-sub-text hover:text-primery-600' />
+      </button>
+    </div>
   )
 }
 
