50caea9d26684d37eac156e99044a1594822acf2
78 mmd 69 <mmdqurani@gmail.com>
2025-05-01 20:20:12 +0330

    Developing the FooterAboutUs component for sizes larger than lg

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='h-max w-full mt-8 bg-white shadow-md shadow-gray-200/60'>
-      <div className='container w-8xl h-full mx-auto flex flex-col bg-red-100'>
+    <footer className='h-max w-full mt-8 bg-white shadow-md shadow-gray-200 py-12'>
+      <div className='container w-8xl h-full mx-auto flex flex-col'>
 
-        <div className='About-us-text-box'>logo & text</div>
+        <FooterAboutUs />
 
         <div className='links-by-pages-box'>links by pages</div>
 
@@ -18,4 +18,24 @@ function Footer() {
   )
 }
 
+export function FooterAboutUs() {
+  return (
+    <div className='footer-About-us grid grid-cols-9'>
+
+      <div className='footer-about-us-box flex-1 px-10 flex flex-col justify-around col-span-8' dir='rtl'>
+        <h3 className='text-main-text-2 font-bold text-xl'>تلاش ما دسترسی آسان تر شما به خدمات پزشکی است</h3>
+
+        <span className='text-sub-text text-sm leading-8'>
+          با افتخار به شما پلتفرم نوبیتو را معرفی میکنیم. ما با افتخار به عنوان یک پلتفرم جامع ارائه دهنده خدمات پزشکی مثل نوبت‌دهی آنلاین، خدمات مشاوره حضوری و غیرحضوری و خدمات پزشکی در منزل را ارائه می‌دهیم. از اختصاص نوبت‌های پزشکی تا امکان مشاوره آنلاین و حضوری، همه چیز در اینجاست تا به شما یک تجربه درمانی راحت و بی دردسر ارائه دهیم.تمام تلاش و سعی ما بر این باور است که دغدغه های دسترسی و درمان را برای بیماران و همراهانشان کمتر کنیم،امیدواریم نوبیتو در روزهای ناخوش احوالی همراه و همیار شما باشد.
+        </span>
+
+      </div>
+
+      <div className='footer-logo-box w-full flex justify-center items-center col-span-1'>
+        <img src="/images/logo/logo-footer.png" alt="" />
+      </div>
+
+    </div>
+  )
+}
 export default Footer
