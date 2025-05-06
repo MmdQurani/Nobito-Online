84f0e0f50db664f27cebe54aee2f59a77fb29fb8
78 mmd 69 <mmdqurani@gmail.com>
2025-05-06 21:12:00 +0330

    Completing the Card_Slide component

---
+++ Components/CardSlide/CardSlide.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { MdOutlineLocationOn } from 'react-icons/md'
+
+function Card_Slide() {
+  return (
+    <div className='Card-Slide h-full w-full flex flex-col justify-start gap-y-2.5 p-4'>
+      <Slide_Image />
+
+      <Slide_Body />
+    </div>
+  )
+}
+
+export function Slide_Image({ srcImg }) {
+  return (
+    <div className='slide-image'>
+      <img src="/images/Doctors/Pic.png" className='object-cover' alt="" />
+    </div>
+  )
+}
+
+export function Slide_Body() {
+  return (
+    <div className='body-slide flex-1 flex flex-col justify-between' dir='rtl'>
+
+      <div className='flex flex-row justify-between items-center mb-2'>
+        <Title_Card />
+
+        <Star_Score />
+      </div>
+
+      <PhysicianExpertiseAndPatientSatisfaction />
+
+      <Doctor_Specialty />
+
+      <hr className='my-1 text-gray-200' />
+
+      <DoctorAddress />
+
+    </div>
+  )
+}
+
+export function Title_Card({ title }) {
+  return (
+    <span className='title-card text-lg text-main-text-2'>محمد ابراهیمی</span>
+  )
+}
+
+export function Star_Score({ score }) {
+  return (
+    <div className='Star-Score flex flex-row items-center text-xs gap-x-1'>
+      <span className='text-main-text-2/70 font-bold font-Dirooz'>4/2</span>
+      <img src="/icons/star.png" className='w-[18px] h-[18px]' alt="" />
+    </div>
+  )
+}
+
+export function PhysicianExpertiseAndPatientSatisfaction({ satisfaction, specialty }) {
+  return (
+    <div className='Physician-expertise-and-patient-satisfaction flex flex-col gap-y-4 items-start mb-2'>
+      <span className='text-sm text-sub-text font-Dirooz font-medium'>متخصص قلب و عروق</span>
+      <span className='text-sm text-primery-600 font-Dirooz font-medium'>(97%) 2374 بیمار راضی</span>
+    </div>
+  )
+}
+
+export function Doctor_Specialty({ specialty }) {
+  return (
+    <div className='Doctor-specialty text-xs text-main-text-3 flex flex-row justify-around'>
+      <span className='bg-gray-50 px-3 py-1 rounded-2xl cursor-pointer'>جراحی قلب</span>
+      <span className='bg-gray-50 px-3 py-1 rounded-2xl cursor-pointer'>آنجوگرافی</span>
+      <span className='bg-gray-50 px-3 py-1 rounded-2xl cursor-pointer'>تست ورزش</span>
+    </div>
+  )
+}
+
+export function DoctorAddress({ address }) {
+  return (
+    <div className='Doctor-address flex flex-row items-center text-main-text-3'>
+      <MdOutlineLocationOn className='text-xl' />
+      <span className='text-xs test-text'>نشانی : تهران _ میدان آرژانتین _ خیابان لاله _ کوچه صاد _ پلاک 18</span>
+    </div>
+  )
+}
+
+export default Card_Slide
--- Components/Slider/Slider.jsx
+++ Components/Slider/Slider.jsx
@@ -10,7 +10,7 @@ import 'swiper/css/pagination';
 
 // import required modules
 import { Pagination } from 'swiper/modules';
-import { MdOutlineLocationOn } from "react-icons/md";
+import Card_Slide from '../CardSlide/CardSlide';
 
 export default function Slider() {
   return (
@@ -25,50 +25,29 @@ export default function Slider() {
         className="mySwiper container"
       >
         <SwiperSlide className='h-full'>
-          <div className='h-full w-full flex flex-col justify-start gap-y-2.5 p-4'>
-            <div className='image-slide'>
-              <img src="/images/Doctors/Pic.png" className='object-cover' alt="" />
-            </div>
-
-            <div className='body-slide flex-1 flex flex-col justify-between' dir='rtl'>
-              <div className='flex flex-row justify-between items-center mb-2'>
-                <span className='text-lg text-main-text-2'>محمد ابراهیمی</span>
-
-                <div className='flex flex-row items-center text-xs gap-x-1'>
-                  <span className='text-main-text-2/70 font-bold font-Dirooz'>4/2</span>
-                  <img src="/icons/star.png" className='w-[18px] h-[18px]' alt="" />
-                </div>
-              </div>
-
-              <div className='flex flex-col gap-y-4 items-start mb-2'>
-                <span className='text-sm text-sub-text font-Dirooz font-medium'>متخصص قلب و عروق</span>
-                <span className='text-sm text-primery-600 font-Dirooz font-medium'>(97%) 2374 بیمار راضی</span>
-              </div>
-
-              <div className='text-xs text-main-text-3 flex flex-row justify-around'>
-                <span className='bg-gray-50 px-3 py-1 rounded-2xl cursor-pointer'>جراحی قلب</span>
-                <span className='bg-gray-50 px-3 py-1 rounded-2xl cursor-pointer'>آنجوگرافی</span>
-                <span className='bg-gray-50 px-3 py-1 rounded-2xl cursor-pointer'>تست ورزش</span>
-              </div>
-
-              <hr className='my-1 text-gray-200' />
-
-              <div className='bg-amber-400'>
-                <MdOutlineLocationOn className='text-2xl' />
-              </div>
-
-            </div>
-          </div>
+          <Card_Slide />
         </SwiperSlide>
-
-        <SwiperSlide className='h-full'>Slide 2</SwiperSlide>
-        <SwiperSlide className='h-full'>Slide 3</SwiperSlide>
-        <SwiperSlide className='h-full'>Slide 4</SwiperSlide>
-        <SwiperSlide className='h-full'>Slide 5</SwiperSlide>
-        <SwiperSlide className='h-full'>Slide 6</SwiperSlide>
-        <SwiperSlide className='h-full'>Slide 7</SwiperSlide>
-        <SwiperSlide className='h-full'>Slide 8</SwiperSlide>
-        <SwiperSlide className='h-full'>Slide 9</SwiperSlide>
+        <SwiperSlide className='h-full'>
+          <Card_Slide />
+        </SwiperSlide>
+        <SwiperSlide className='h-full'>
+          <Card_Slide />
+        </SwiperSlide>
+        <SwiperSlide className='h-full'>
+          <Card_Slide />
+        </SwiperSlide>
+        <SwiperSlide className='h-full'>
+          <Card_Slide />
+        </SwiperSlide>
+        <SwiperSlide className='h-full'>
+          <Card_Slide />
+        </SwiperSlide>
+        <SwiperSlide className='h-full'>
+          <Card_Slide />
+        </SwiperSlide>
+        <SwiperSlide className='h-full'>
+          <Card_Slide />
+        </SwiperSlide>
       </Swiper>
     </>
   );
